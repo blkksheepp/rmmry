8ebb40a225965e9322408f9ccefe3ee9e174080d
Harsh <harshraj91105@gmail.com>
2025-11-25 05:04:01 +0530

    feat: overview section, better copy

--- src/components/Overview/index.tsx
+++ src/components/Overview/index.tsx
@@ -14,16 +14,13 @@ const BgPatterns = () => {
             {/* Top Left Extension lines */}
             <line x1="25%" y1="0" x2="25%" y2="100%" stroke="white" strokeWidth="0.5" strokeDasharray="4 4" opacity="0.5" />
             <line x1="0" y1="28%" x2="100%" y2="28%" stroke="white" strokeWidth="0.5" strokeDasharray="4 4" opacity="0.5" />
-            {/* Bottom Right Extension lines */}
-            {/* <line x1="60%" y1="0%" x2="97%" y2="139%" stroke="white" strokeWidth="0.5" opacity="0.5" /> */}
-            {/* <line x1="0%" y1="100%" x2="139%" y2="0%" stroke="white" strokeWidth="0.5" opacity="0.5" /> */}
         </svg>
     )
 }
 
 const Overview = () => {
     return (
-        <div id="overview" className="h-full lg:h-screen lg:overflow-y-hidden bg-primary select-none">
+        <div id="overview" className="h-full lg:h-screen lg:overflow-y-hidden bg-primary select-none scroll-mt-16">
 
             {/* Main Content */}
             <div className="h-full lg:flex-1 lg:flex lg:flex-row">
@@ -33,19 +30,24 @@ const Overview = () => {
                         {/* Nav Header */}
                         <div className="flex flex-row items-center ~gap-16/32 pt-4 pb-6 font-light ~text-base/lg uppercase">
                             <p className="text-foreground">01</p>
-                            <p className="text-background bg-foreground px-4 rounded-full">Overview</p>
+                            <p className="text-background bg-foreground px-4 rounded-full">The Problem</p>
                         </div>
 
                         {/* Heading */}
-                        <p className="text-foreground ~text-4xl/5xl uppercase w-[85%] lg:w-[70%]">How RMMRY Uses Your Data Is Simple.</p>
+                        <p className="text-foreground ~text-4xl/5xl uppercase w-[85%] lg:w-[70%]">The tools you use aren’t built for keeping your work accessible.</p>
                     </div>
 
                     {/* Description */}
                     <div>
                         {/* Description Heading */}
-                        <p className="text-foreground ~text-5xl/6xl uppercase mr-10">Upload Your Data</p>
+                        <p className="text-foreground ~text-5xl/6xl uppercase mr-10">Lost Prompts, Lost Time</p>
                         {/* Description Content */}
-                        <p className="~mt-2/4 leading-relaxed text-foreground ~text-base/lg uppercase w-[90%] md:w-[80%] lg:w-[75%] xl:w-[60%]">upload your model to rmmry portal or 3rd party data management system. we work with you to integrate your project schedule. schedule of values, and any other metadata that matters. upload your model to rmmry portal or 3rd party data management system.</p>
+                        <p className="~mt-2/4 leading-relaxed text-foreground ~text-base/lg uppercase w-[90%] md:w-[80%] lg:w-[75%] xl:w-[60%]">
+                            Most LLMs bury your past prompts and scatter them across chats. 
+                            Search is unreliable and slow when you need something instantly. 
+                            Important work gets lost, forgotten, or trapped in long conversations. 
+                            You shouldn’t have to rewrite the same prompt twice.
+                        </p>
 
                         {/* Nav Dots */}
                         <div className="flex flex-row gap-2.5 mt-8">
@@ -66,16 +68,16 @@ const Overview = () => {
                     </div>
 
                     {/* Heading */}
-                    <p className="~pr-6/10 lg:pr-0 self-end lg:self-start text-foreground ~text-4xl/5xl uppercase">Step 1/4</p>
+                    <p className="pr-4 sm:~pr-6/10 lg:pr-0 self-end lg:self-start text-foreground ~/md:~text-3xl/5xl sm:~text-4xl/5xl uppercase">The Problematic UX</p>
 
                     {/* Media */}
-                    <div className="mt-8 w-full h-full relative">
+                    <div className="~mt-4/8 w-full h-full relative">
                         {/* Image */}
 
                         <img src={contentImg} alt="Overview Media 1" className="absolute w-full h-full rotate-[180deg] object-cover" />
 
                         {/* Button */}
-                        <button className="absolute w-[350px] bottom-24 left-1/2 translate-x-[-50%] flex justify-between items-center bg-background text-foreground px-4 py-0.5 rounded-full uppercase">
+                        <button className="absolute w-[350px] bottom-24 left-1/2 translate-x-[-50%] flex justify-between items-center bg-[#e6eef1] text-[#0f1724] px-4 py-0.5 rounded-full uppercase">
                             <span>Learn More</span>
                             <Plus className="px-0.5 py-0.5 rounded-full h-auto w-7" strokeWidth={1.5} />
                         </button>
